f63c354400996848f8171a89842d8ba11608ea1b
Xsimple1010 <aderval758@gmail.com>
2025-07-13 13:41:08 -0300

    adiciona rota de ping ao servidor

--- src/server/mod.rs
+++ src/server/mod.rs
@@ -1,12 +1,20 @@
-use axum::{Router, extract::DefaultBodyLimit, routing::post};
+use axum::{
+    Router,
+    extract::DefaultBodyLimit,
+    routing::{get, post},
+};
 use tokio::net::TcpListener;
 
 use crate::{
     AppState,
-    server::download_exe::{download_dep, download_exe},
+    server::{
+        download_exe::{download_dep, download_exe},
+        ping::ping,
+    },
 };
 
 mod download_exe;
+mod ping;
 
 pub async fn init_server(state: AppState) {
     let flash_port = std::env::var("FLASH_CLIENT_PORT").unwrap_or_else(|_| "4090".to_string());
@@ -16,6 +24,7 @@ pub async fn init_server(state: AppState) {
     let app = Router::new()
         .route("/exe", post(download_exe))
         .route("/dep", post(download_dep))
+        .route("/ping", get(ping))
         .with_state(state)
         .layer(DefaultBodyLimit::max(600 * 1024 * 1024)); // Limite de 600MB por upload
 
